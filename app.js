ef309a78a3c8d144ede1b346a973af2b3a4733d4
shariq <shariq@shariq-X401A1.(none)>
2014-07-08 22:56:15 -0400

    merged my local updates

--- app.js
+++ app.js
@@ -1,88 +1,109 @@
 var express = require('express');
 var http = require('http');
 var app = express();
-var server = http.createServer(app);
-var socket = require('socket.io').listen(server);
 var map_util = require('./maputils/index.js');
+
+
+
 app.use(express.static(__dirname + "/public"));
 app.use("/packages", express.static(__dirname + "/bower_components"));
 app.use(express.bodyParser());
+app.use("views", express.static(__dirname + "/views"));
+app.use(app.router);
+app.use(function(err, req, res, next){
+	res.status(500);
+	res.render("error.html", {error: err});
+});
+
 app.engine('html', require('ejs').renderFile);
 
+
+
+
+
 app.get('/', function(req, res){
+	res.render('login.html');
+});
+
+app.post('/login', function(req, res){
+	if(req.body.username !== 'richmond804' && req.body.password !== 'this is for code for america'){
+		res.render('error.html');
+	}else{
+		res.redirect('/form');
+	}
+});
+
+app.get('/form', function(req, res){
 	res.render('index.html');
 });
 
-app.post('/find', function(req, res){
-	var route_list = [];
-	var myLocation = map_util.normalizeLatLng([-77.520968,37.513003]);
-	var nearestBusStop = map_util.normalizeLatLng([-77.520626, 37.512143]);
 
-	//get the routes where that bus stop belongs to
-	var routes_coordinates = [
-		{time: '19:02', loc:[-77.517848,37.511092], type:'start'},
-		{time: '19:10', loc:[-77.519779,37.510266], type:'stop'},
-		{time: '19:12', loc:[-77.520626, 37.512143], type:'stop'},
-		{time: '19:14', loc:[-77.522971,37.515355], type:'stop'},
-		{time: '19:15', loc:[-77.524939,37.51584], type:'dest'}
-	];
-	for(var i = 0; i < routes_coordinates.length; i++){
-		routes_coordinates[i].loc = map_util.normalizeLatLng(routes_coordinates[i].loc);
+app.post('/find_address', function(req, res, next){
+	var app_res = res;
+	var app_req = req;
+	var app_next = next;
+	var	myLocation, nearestBusStop, nearestBusStopAddress;
+	var routes_coordinates = [];
+	var school = req.body.school;
+	if(req.body.address.match(/[ /](richmond|Richmond),[ /](va|VA)/i)) {
+		var address = req.body.address;
+	}else{
+		var address = req.body.address + ' ,Richmond, VA';
 	}
+	var clientHours = req.body.hours;
+	console.log(clientHours);
+	//we need to geocode the user's address. 
+	var geoAddressData= '';
+	var geocodeAPI = "http://www.datasciencetoolkit.org/maps/api/geocode/json?sensor=false&address=" + encodeURIComponent(address);
+	var geoAddress = http.get(geocodeAPI, function(res){
+		res.on('data', function(chunk){
+			geoAddressData += chunk;
+		});
 
-	res.render('results.html', {myBusStop: nearestBusStop, myLocation: myLocation, routes_coordinates: routes_coordinates} );
-});
-
-app.post('/find_address', function(req, res){
-	var app_res = res,
-			app_req = req,
-			myLocation, nearestBusStop, nearestBusStopAddress,
-			routes_coordinates = [],
-			school = req.body.school,
-			address = req.body.address;
-
-	//we need to geocode the user's address.
-	var geoAddressData = '',
-			geocodeAPI = "http://www.datasciencetoolkit.org/maps/api/geocode/json?sensor=false&address=" + encodeURIComponent(address),
-			geoAddress = http.get(geocodeAPI, function(res) {
-				res.on('data', function(chunk){
-					geoAddressData += chunk;
-				});
-
-		res.on('end', function() {
+		res.on('end', function(){
 			geoAddressData = JSON.parse(geoAddressData);
-			myLocation = [geoAddressData.results[0].geometry.location.lat, geoAddressData.results[0].geometry.location.lng];
+			myLocation = [geoAddressData.results[0].geometry.location.lat, 
+								geoAddressData.results[0].geometry.location.lng];
 
-			map_util.findNearestBusStop(geoAddressData, school, function(err, data) {
-				nearestBusStop = map_util.normalizeLatLng(data[0].loc);
-				nearestBusStopAddress = data[0].address;
-
-				// then get the route
-				// but first let's see if we get the morning or afternoon route
-				if(new Date().getHours() < 8){
-					// P is for pickup
-					var route_num = data[0].route_num.slice(0,5) + "P" + data[0].route_num.slice(-2);
-				}else{
-					// D is for dropoff
-					var route_num = data[0].route_num.slice(0,5) + "D" + data[0].route_num.slice(-2);
-				}
-				map_util.getRoute(route_num, function(err, data){
-					console.log(data);
-					routes_coordinates = data;
-					for(var i = 0; i < routes_coordinates.length; i++){
-						routes_coordinates[i].loc = map_util.normalizeLatLng(routes_coordinates[i].loc);
+			
+			map_util.findNearestBusStop(geoAddressData, clientHours, school, function(err, data){
+					if(err){
+						var error = err;
+						console.log(err);
+						app_next(error);
+					}else if(data.length == 0){
+						app_res.render('no_results.html');
+					}else{
+						console.log(data[0]);
+						nearestBusStop = map_util.normalizeLatLng(data[0].loc);
+						nearestBusStopAddress = data[0].address;
+						//then get the route
+						//but first let's see if we get the morning or afternoon route
+						console.log("the hours is " + clientHours);
+						var route_num = data[0].route_num;
+						
+						map_util.getRoute(route_num, function(err, data){
+							console.log(data);
+							routes_coordinates = data;
+							for(var i = 0; i < routes_coordinates.length; i++){
+								routes_coordinates[i].loc = map_util.normalizeLatLng(routes_coordinates[i].loc);
+							}
+							app_res.render('results.html', {myBusStop: nearestBusStop, nearestBusStopAddress: nearestBusStopAddress, myLocation: myLocation, routes_coordinates: routes_coordinates});
+						});
 					}
-					app_res.render('results.html', {myBusStop: nearestBusStop, nearestBusStopAddress: nearestBusStopAddress, myLocation: myLocation, routes_coordinates: routes_coordinates});
-				});
 			});
 		});
 	}).on('error', function(e){
-		console.error(e);
+		console.log(e);
+		app_next(new Error("Could not connect to geocoding service!"));
 	});
 
-});
+});	
 
 
-server.listen(8080);
+
+
+
+app.listen(10817);
 
 
