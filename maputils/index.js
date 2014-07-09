2b5d46e2630851f67717e1874bdbd343349d6f01
shariq <shariq@shariq-X401A1.(none)>
2014-07-08 23:14:42 -0400

    took out the db_uri in the connect func

--- maputils/index.js
+++ maputils/index.js
@@ -1,6 +1,6 @@
 var mquery = require('mquery');
 var mongo = require('mongodb').MongoClient;
-
+var mongo_cred = require('../db/index.js');
 
 
 exports.normalizeLatLng = function(coordinates){
@@ -12,29 +12,36 @@ exports.normalizeLatLng = function(coordinates){
 
 };
 
-exports.findNearestBusStop = function(geoInfo, school, cb){
-	mongo.connect('mongodb://xxxx:xxxx@xxxxx/busstops', function(err, db){
-		if(err) throw err;
-		console.log(school);
-		var busstops = db.collection('routes');
+exports.findNearestBusStop = function(geoInfo, clientHours, school, cb){
+	mongo.connect(mongo_cred.db_uri, function(err, db){
+		if(err){
+		 cb(err, []);
+		}else{
+			console.log(school);
+			var busstops = db.collection('routes');
+			var letter;
+			(clientHours < 8) ? letter = "P" : letter = "D";
+			console.log(letter);
 
-		mquery(busstops).where('loc').near({ center: [geoInfo.results[0].geometry.location.lng, geoInfo.results[0].geometry.location.lat], 
-			spherical: true, maxDistance: 0.3 })
-				.where('school').regex('.*' + school +'.*')
-				.where('type').equals('STOP')
-				.exec(function(err, docs){
-					var error = null;
-					if(err) error = err;
-					console.log('found bus stop!');
-					db.close();
-					cb(error, docs);
-				});
+			mquery(busstops).where('loc').near({ center: [geoInfo.results[0].geometry.location.lng, geoInfo.results[0].geometry.location.lat], 
+				spherical: true, maxDistance: 0.3 })
+					.where('school').regex('.*' + school +'.*')
+					.where('route_num').regex(letter)
+					.where('type').equals('STOP')
+					.exec(function(err, docs){
+						var error = null;
+						if(err) error = err;
+						console.log('found bus stop!');
+						db.close();
+						cb(error, docs);
+			});
+		}		
 	});
 }
 
 
 exports.getRoute = function(routeLabel, cb){
-	mongo.connect('mongodb://xxxx:xxxxx@xxxxxx/busstops', function(err, db){
+	mongo.connect(mongo_cred.db_uri, function(err, db){
 		if(err) throw err;
 		console.log(routeLabel);
 		var busstops = db.collection('routes');
